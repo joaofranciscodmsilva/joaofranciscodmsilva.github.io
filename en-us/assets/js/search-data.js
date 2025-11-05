f32e6ce4775c6a30595580f02fe087ec2df38aee
joaofranciscodmsilva <80005218+joaofranciscodmsilva@users.noreply.github.com>
2025-11-05 00:26:52 +0000

    Deploying to gh-pages from @ joaofranciscodmsilva/joaofranciscodmsilva.github.io@098a3154ffa32dc504b53732700a7685c4958b59 🚀

--- en-us/assets/js/search-data.js
+++ en-us/assets/js/search-data.js
@@ -14,8 +14,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-sobre",
-    title: "Sobre",
+    id: "nav-about",
+    title: "About",
     section: "Navigation menu",
     handler: () => {
       window.location.href = "/en-us/";
